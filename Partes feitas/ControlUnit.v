c24ed1c1a4b3fb92bd5806b8f54cae7deb520d6d
RodrigoMesel <rmlo@cin.ufpe.br>
2022-03-31 14:17:12 -0300

    a

--- Partes feitas/ControlUnit.v
+++ Partes feitas/ControlUnit.v
@@ -34,15 +34,88 @@ module ControlUnit (
     parameter rst = 6'b000000;
     parameter fetch = 6'b000001;
     parameter decode = 6'b000010;
-    parameter op404 = 6'b000_011;
-    parameter overflow = 6'b000_100;
-    parameter zerodiv = 6'b000_101;
+    parameter op404 = 6'b000011;
+    parameter overflow = 6'b000100;
+    parameter zerodiv = 6'b000101;
 
-    parameter ADD = 6'b000_110;
+    parameter ADD = 6'b000110;
+    parameter AND = 6'b000111;
+    parameter DIV = 6'b001000;
+    parameter MULT = 6'b001001;
+    parameter JR = 6'b001010;
+    parameter MFHI = 6'b001011;
+    parameter MFLO = 6'b001100;
+    parameter SLL = 6'b001101;
+    parameter SLLV = 6'b001111;
+    parameter SLT = 6'b010000;
+    parameter SRA = 6'b010001;
+    parameter SRAV = 6'b010010;
+    parameter SRL = 6'b010011;
+    parameter SUB = 6'b010100;
+    parameter BREAK = 6'b010101;
+    parameter RTE = 6'b010110;
+    parameter ADDM = 6'b010111;
+    parameter ADDI = 6'b011000;
+    parameter ADIIU = 6'b011001;
+    parameter BEQ = 6'b011010;
+    parameter BNE = 6'b011011;
+    parameter BLE = 6'b011100;
+    parameter BGT = 6'b011101;
+    parameter SLLM = 6'b011110;
+    parameter LB = 6'b011111;
+    parameter LH = 6'b100000;
+    parameter LUI = 6'b100001;
+    parameter LW = 6'b100010;
+    parameter SB = 6'b100011;
+    parameter SH = 6'b100100;
+    parameter SLTI = 6'b100101;
+    parameter SW = 6'b100111;
+    parameter J = 6'b101000;
+    parameter JAL = 6'b101001;
 
+    //R Instructions
     parameter opcodeR = 6'b000000;
     
     parameter ADDFunct = 6'b100000;
+    parameter ANDFunct = 6'b100100;
+    parameter DIVFunct = 6'b011010;
+    parameter MULTFunct = 6'b011000;
+    parameter JRFunct = 6'b001000;
+    parameter MFHIFunct = 6'b010000;
+    parameter MFLOFunct = 6'b010010;
+    parameter SLLFunct = 6'b000000;
+    parameter SLLVFunct = 6'b000100;
+    parameter SLTFunct = 6'b101010;
+    parameter SRAFunct = 6'b000011;
+    parameter SRAVFunct = 6'b000111;
+    parameter SRLFunct = 6'b000010;
+    parameter SUBFunct = 6'b100010;
+    parameter BREAKFunct = 6'b001101;
+    parameter RTEFunct = 6'b010011;
+    parameter ADDMFunct = 6'b000101;
+
+    //I Instructions
+
+    parameter ADDIop = 6'b001000;
+    parameter ADDIUop = 6'b001001;
+    parameter BEQop = 6'b000100;
+    parameter BNEop = 6'b000101;
+    parameter BLEop = 6'b000110;
+    parameter BGTop = 6'b000111;
+    parameter SLLMop = 6'b000001;
+    parameter LBop = 6'b100000;
+    parameter LHop = 6'b100001;
+    parameter LWop = 6'b100011;
+    parameter SBop = 6'b101000;
+    parameter SHop = 6'b101001;
+    parameter SWop = 6'b101011;
+    parameter SLTIop = 6'b001010;
+    parameter LUIop = 6'b001111;
+
+    //J Instructions
+
+    parameter Jop = 6'b000010;
+    parameter JALop = 6'b000011;
 
     reg [5:0] state;
     reg [4:0] counter;
@@ -113,10 +186,42 @@ module ControlUnit (
                         opcodeR:
                             case(Funct)
                                 ADDFunct: state = ADD;
-                                //...
+                                ANDFunct: state =  AND;
+                                DIVFunct: state =  DIV;
+                                MULTFunct: state =  MULT;
+                                JRFunct: state =  JR;
+                                MFHIFunct: state =  MFHI;
+                                MFLOFunct: state =  MFLO;
+                                SLLFunct: state =  SLL;
+                                SLLVFunct: state =  SLLV;
+                                SLTFunct: state =  SLT;
+                                SRAFunct: state =  SRA;
+                                SRAVFunct: state =  SRAV;
+                                SRLFunct: state =  SRL;
+                                SUBFunct: state =  SUB;
+                                BREAKFunct: state =  BREAK;
+                                RTEFunct: state =  RTE;
+                                ADDMFunct: state =  ADDM;
 
                             endcase
-                        //...
+                        ADDIop: state = ADDI;
+                        ADDIUop: state = ADDIU;
+                        BEQop: state = BEQ;
+                        BNEop: state = BNE;
+                        BLEop: state = BLE;
+                        BGTop: state = BGT;
+                        SLLMop: state = SLLM;
+                        LBop: state = LB;
+                        LHop: state = LH;
+                        LWop: state = LW;
+                        SBop: state = SB;
+                        SHop: state = SH;
+                        SWop: state = SW;
+                        SLTIop: state = SLTI;
+                        LUIop: state = LUI;
+
+                        Jop: state = J;
+                        JALop: state = JAL;
                     endcase
 
                 end
